bfa86ec42866c5ec386106bf9e6c4062e19815c4
Edgar <edgar@imac-van-backer.home>
2023-03-15 16:27:32 +0100

    loading

--- GestureAndAnimations/ViewController.swift
+++ GestureAndAnimations/ViewController.swift
@@ -15,5 +15,19 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        //print("moving")
+        guard let viewToPan = sender.view else {return}
+        
+        let translation = sender.translation(in: self.view)
+        
+        viewToPan.center = CGPoint(x: viewToPan.center.x + translation.x, y: viewToPan.center.y + translation.y)
+        
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @IBAction func catTapped(_ sender: UITapGestureRecognizer) {
+        print("cat tapped")
+    }
 }
 
